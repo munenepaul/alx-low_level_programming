b39a7a82851dc042ad4999a41d4854dbd5c53264
munenepaul <evansmunenepaul@gmail.com>
2023-02-16 13:57:02 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
+
 /**
  * main- prints the given input and returns with value
- * Return (1) (sucess)
+ * Return: Always 0 (sucess)
  * int main -returns value of an interger
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
