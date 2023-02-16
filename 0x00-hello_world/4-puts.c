8ea5e0d4a32c45c12e857689386adfd6a98c55e6
munenepaul <evansmunenepaul@gmail.com>
2023-02-16 07:21:37 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * main -This program will use puts function to return a value
+ * Return: 0 becomes successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
