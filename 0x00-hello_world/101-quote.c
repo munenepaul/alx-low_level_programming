4884686cceea0a92d11d504382670e1fa18e0d00
munenepaul <evansmunenepaul@gmail.com>
2023-02-16 13:50:09 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- prints the given input and returns with value
+ * Return (1) (sucess)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
