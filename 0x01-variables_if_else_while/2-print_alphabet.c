6c077fbd56339fa096fc55afe586e204b9e8553b
munenepaul <evansmunenepaul@gmail.com>
2023-02-17 05:34:49 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description-a program that prints the alphabet in lowercase
+ * Return: (0) always (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c >= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
