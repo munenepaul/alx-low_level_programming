671c12df82302df78bb026b018941015227c8bb5
munenepaul <evansmunenepaul@gmail.com>
2023-02-21 09:32:27 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_ alphabet-prints the alpha in lowercase
+ */
+void print_alphabet(void)
+
+{
+	char za;
+
+	for (za = 'a'; za <= 'z'; za++)
+	{
+		_putchar(za);
+	}
+	_putchar('\n');
+}
