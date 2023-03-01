0cd005a150075d64dbbb46067a4339a12656ebfd
munenepaul <evansmunenepaul@gmail.com>
2023-03-01 02:11:04 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string to modify
+ * Return: return the results
+ */
+char *string_toupper(char *s)
+{
+	int k;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+		if (s[k] >= 'a' && s[k] <= 'z')
+			s[k] = s[k] - 32;
+	}
+	return (s);
+}
