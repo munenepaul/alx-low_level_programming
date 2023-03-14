65c58e433f4d2f2fdc9ba69e2f9c0d65abbc9241
munenepaul <evansmunenepaul@gmail.com>
2023-03-14 11:32:32 -0700

    task done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,16 +15,16 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *iArray;
 
-	if (size = 0)
+	if (size == 0)
 		return (NULL);
-	/* returns a pointer to the allocated memory */
 
-	iArray = malloc(size *sizeof(char));
+	/* returns a pointer to the allocated memory */
+	iArray = malloc(size * sizeof(char));
 
 	if (iArray == NULL)
 		return (NULL);
 
-			i = 0;
+	i = 0;
 	while (i < size)
 	{
 		iArray[i] = c;
